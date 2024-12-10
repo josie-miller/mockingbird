3660b163851beba1408b712d48badf61d3ef8ebb
josie-miller <josephinemiller13524@icloud.com>
2024-12-09 19:12:20 -0800

    suicide

--- proj1963/settings.py
+++ proj1963/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xbm-vgegk^6-ctos8616h8+46eo*aakb!i9v#)%f@!ixd%uriv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['josiemiller101.pythonanywhere.com']
 
